ec445c4ace26a4a91a06ac7bd4dfb9f19af0c627
Caueluis <caue.ls@hotmail.com>
2021-04-16 16:29:01 -0300

    inicio da aplicação

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+import { RepositoryList } from './components/RepositoryList';
+
 import './styles/global.scss';
 
 export function App() {
+    return <RepositoryList />
 
-
-    return <h1>Hello ReactJs</h1>
 }
---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,37 @@
+export function RepositoryList(){
+    return (
+        <section className="repository-list">
+            <h1>Lista de repositórios</h1>
+
+            <ul>
+                <l1>
+                    <br></br><strong>Unform</strong>
+                    <p>Forms in React</p>
+
+                    <a href="">
+                        Acessar repositório
+                    </a>
+                </l1>
+
+                <l1>
+                <br></br><strong>Unform</strong>
+                    <p>Forms in React</p>
+
+                    <a href="">
+                        Acessar repositório
+                    </a>
+                </l1>
+
+                <l1>
+                    <br></br><strong>Unform</strong>
+                    <p>Forms in React</p>
+
+                    <a href="">
+                        Acessar repositório
+                    </a>
+                </l1>
+            </ul>
+
+        </section>
+    );
+}
